6d858e6dfca03435123809c2a984971c41cc7de2
Artur Serra <artur_seilah@hotmail.comm>
2023-02-08 16:21:57 -0300

    Setup the config, hooked with Sanity Studio backend

---
+++ app/head.tsx
@@ -0,0 +1,9 @@
+export default function Head() {
+  return (
+    <>
+      <title>The Rusty Code</title>
+      <meta content="width=device-width, initial-scale=1" name="viewport" />
+      <link rel="icon" href="/favicon.ico" />
+    </>
+  )
+}
---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+export default function HomePage() {
+    return (
+        <div>
+            <h1 className="text-4xl">
+                Welcome to The Rusty Code!
+            </h1>
+            <p>
+                Te amo, Masha!
+            </p>
+        </div>
+    )
+}
