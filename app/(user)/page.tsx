e282653f2640d861b3cfceb98983cc82c7ddd6e9
Artur Serra <artur_seilah@hotmail.com>
2023-02-14 13:58:31 -0300

    Added a preview mode for checking blog posts before posting them.

--- app/(user)/page.tsx
+++ app/(user)/page.tsx
@@ -1,6 +1,22 @@
+import { previewData } from "next/headers";
+import { groq } from "next-sanity";
+
+const query = groq`
+    
+`
+
 export default function HomePage() {
+    if (previewData()) {
+        return (
+            <div>
+                Preview Mode
+            </div>
+        )
+    }
+
     return (
         <div>
+            Regular mode
         </div>
     )
 }
