872a71740fcb4acfd1a22e0664106f96c378d8a7
Artur Serra <artur_seilah@hotmail.com>
2023-02-20 15:45:54 -0300

    Fixed the categories display on the homepage. Styles still broken

--- components/BlogList.tsx
+++ components/BlogList.tsx
@@ -1,89 +1,94 @@
 import Image from "next/image";
 import urlFor from "../lib/urlFor";
-import category from "../schemas/category";
 
 type Props = {
-    posts: Post[];
+  posts: Post[];
 };
 
 function BlogList({ posts }: Props) {
   return (
     <div>
-        <hr 
-          className="
-            border-[#F7AB0A]
-            mb-10
-          "
-        />
+      <hr
+        className="
+          border-[#F7AB0A]
+          mb-10
+        "
+      />
 
-        <div>
-          { posts.map(post => (
-            <div 
-              key={ post._id }
+      <div>
+        {posts.map((post) => (
+          <div
+            key={post._id}
+            className="
+              flex
+              flex-col
+              group
+              cursor-pointer
+            "
+          >
+            <div
               className="
-                flex
-                flex-col
-                group 
-                cursor-pointer
+                relative
+                w-full
+                h-80
+                drop-shadow-xl
+                group-hover:scale-105
+                transition-transform
+                duration-200
+                ease-out
               "
             >
-              <div 
+              <Image
                 className="
-                  relative 
-                  w-full 
-                  h-80
-                  drop-shadow-xl
-                  group-hover:scale-105
-                  transition-transform
-                  duration-200
-                  ease-out
+                  object-cover
+                  object-left
+                  lg:object-center
+                "
+                src={urlFor(post.mainImage).url()}
+                alt={post.author.name}
+                fill
+              />
+              <div
+                className="
+                  absolute
+                  bottom-0
+                  w-full
+                  bg-opacity-20
+                  bg-black
+                  backdrop-blur-lg
+                  rounded
+                  drop-shadow-lg
+                  text-white
+                  p-5
+                  justify-between
                 "
               >
-                <Image 
-                  className="
-                    object-cover
-                    object-left
-                    lg:object-center
-                  "
-                  src={ urlFor(post.mainImage).url() }
-                  alt={ post.author.name }
-                  fill
-                />
-                <div 
-                  className="
-                    absolute 
-                    bottom-0 
-                    w-full
-                    bg-opacity-20
-                    bg-black
-                    backdrop-blur-lg
-                    rounded
-                    drop-shadow-lg
-                    text-white
-                    p-5
-                    justify-between
-                  "
-                >
-                  <div>
-                    <p className="font-bold">
-                      { post.title }
-                    </p>
+                <div>
+                  <p className="font-bold">{post.title}</p>
 
-                    <p>
-                      { new Date(post._createdAt).toLocaleDateString(
-                        "en-US", {
-                          day: "numeric",
-                          month: "long",
-                          year: "numeric",
-                        
-                        })}
-                    </p>
-                  </div>
+                  <p>
+                    {new Date(post._createdAt).toLocaleDateString("en-US", {
+                      day: "numeric",
+                      month: "long",
+                      year: "numeric",
+                    })}
+                  </p>
+                </div>
+
+                <div>
+                  { post.categories?.map((category) => (
+                    <div className="bg-[#F7AB0A] text-center text-black 
+                                    px-3 py-1 rounded-full text-sm font-semibold"
+                    >
+                      { category.title }
+                    </div>
+                  ))}
                 </div>
               </div>
             </div>
-          ))}
-        </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
